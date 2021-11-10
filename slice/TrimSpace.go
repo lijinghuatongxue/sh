52dc5fcb9f60b04aec6b646882a7b1ac527b81ae
lijinghuatongxue <woshilijinghua@gmail.com>
2021-11-10 23:51:57 +0800

    slice: add filter

---
+++ slice/TrimSpace.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"github.com/sirupsen/logrus"
+)
+
+// TrimSpace 消除byte切片中的空格元素
+func TrimSpace(s []byte) []byte {
+	b := s[:0]
+	for _,i := range s{
+		logrus.Info(i)
+		// 32 = ' ' // https://baike.baidu.com/item/ASCII/309296
+		if i != 32{
+			b = append(b,i)
+		}
+		//if i != ' '{
+		//	b = append(b,i)
+		//}
+	}
+	return  b
+}
+func main()  {
+	s := []byte{'1',1,2, ' ', 5}
+	result := TrimSpace(s)
+	fmt.Println(result)
+}
