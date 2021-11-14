39b5b6b47b9ba1fb5751d5798f50e7eb373a4c6f
lijinghuatongxue <woshilijinghua@gmail.com>
2021-11-14 23:20:09 +0800

    fea: learn goroutine  sync.mutex channel的并发模式 例子补充

---
+++ goroutine/并发模式/hello_world_channel_many_demo.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var done = make(chan int, 10)
+	for i := 0; i < cap(done); i++ {
+		go func() {
+			fmt.Println("hello world")
+		}()
+		done <- 1
+	}
+	for i := 0; i < cap(done); i++ {
+		<-done
+	}
+}
