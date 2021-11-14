f5bcfa232d081cbbabbe4fe2789a1382356d71ed
lijinghuatongxue <woshilijinghua@gmail.com>
2021-11-14 20:37:51 +0800

    fea: learn goroutine 基于通道的通信 控制通道的缓存大小 page48

--- goroutine/基于通道的通信/demo2.go
+++ goroutine/基于通道的通信/demo2.go
@@ -4,9 +4,9 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-// 控制通道的缓存大小 page48
+// 控制通道的缓存大小,limit = 2,意味着当前并发执行的goroutine的最大数目 page48
 func main() {
-	var limit = make(chan int, 10)
+	var limit = make(chan int, 2)
 	c := [...]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
 	for _, w := range c {
 		w := w
