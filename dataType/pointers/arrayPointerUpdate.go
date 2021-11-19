b0005f8fa9fccfb0a775cdfc5cab14bb7278a5a6
lijinghuatongxue <woshilijinghua@gmail.com>
2021-11-19 22:30:49 +0800

    fea: learn pointer ｜ add pointer 对函数内的数组指针、切片进行一些修改

--- dataType/pointers/arrayPointerUpdate.go
+++ dataType/pointers/arrayPointerUpdate.go
@@ -2,14 +2,21 @@ package main
 
 import "fmt"
 
-func arrayUpdate(arr *[3]int) {
+func arrayPointerUpdate(arr *[3]int) {
 	(*arr)[0] = 2
+}
+
+func slicePointerUpdate(slice []int) {
+	slice[0] = 3
 }
 
 // 对函数内的数组进行一些修改
 func main() {
 	var a = [3]int{1, 2, 3}
-	fmt.Println("修改前 a: ", a)
-	arrayUpdate(&a)
-	fmt.Println("修改后 a: ", a)
+	fmt.Println("array 数组 修改前 a: ", a)
+	arrayPointerUpdate(&a)
+	fmt.Println("array 数组 修改后 a: ", a)
+	slicePointerUpdate(a[:])
+	fmt.Println("slice 切片 修改后 a: ", a)
+
 }
