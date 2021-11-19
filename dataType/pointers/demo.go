cb54e0ba36ca4f4cd5af798a0b81c368004b2b22
lijinghuatongxue <woshilijinghua@gmail.com>
2021-11-19 08:13:19 +0800

    fea: learn pointer ｜ add pointer demo

--- dataType/pointers/demo.go
+++ dataType/pointers/demo.go
@@ -15,4 +15,9 @@ func main() {
 
 	/* 使用指针访问值，获取指针的值 */
 	fmt.Printf("*ip 变量的值: %d\n", *ip)
+
+	// 改变指针的值, 原变量也会跟着改变
+	*ip = *ip + 1
+	fmt.Printf("*ip 改变后的指针的值: %d\n", *ip)
+	fmt.Printf("a   改变后原变量的值: %d\n", a)
 }
