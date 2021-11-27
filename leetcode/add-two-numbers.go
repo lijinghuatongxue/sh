d9b3b518a504e0bf683d16673003f8ec42d5c6e4
lijinghuatongxue <woshilijinghua@gmail.com>
2021-11-27 17:08:15 +0800

    leetcode: 两链表相加

--- leetcode/add-two-numbers.go
+++ leetcode/add-two-numbers.go
@@ -2,21 +2,34 @@ package main
 
 import (
 	"fmt"
+	"github.com/sirupsen/logrus"
 	"math"
 )
 
 // https://leetcode-cn.com/problems/add-two-numbers/
 
 func main() {
-	V := [...]int{2, 4, 9, 5, 6, 4, 9}
+	V := [...]int{9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9}
 	var l1 = new(ListNode)
 	l1.Val = V[0]
 	var l2 = new(ListNode)
 	l2.Val = V[1]
 	var l3 = new(ListNode)
+	var l4 = new(ListNode)
+	var l5 = new(ListNode)
+	var l6 = new(ListNode)
+	var l7 = new(ListNode)
 	l3.Val = V[2]
+	l4.Val = V[1]
+	l5.Val = V[1]
+	l6.Val = V[1]
+	l7.Val = V[1]
 	l1.Next = l2
 	l2.Next = l3
+	l3.Next = l4
+	l4.Next = l5
+	l5.Next = l6
+	l6.Next = l7
 	// 第2条链表
 	var L1 = new(ListNode)
 	L1.Val = V[3]
@@ -42,15 +55,10 @@ type ListNode struct {
 }
 
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-	var l1Len, LastSum, l2Len, compareValue int
-	var tail, EndL1, EndL2, initLinkNode *ListNode
-	//L1 = l1
-	//EndL1 = L1
-	//L2 = l2
-	EndL2 = l2
-	EndL1 = l1
-	//tail = ListNode
-	//fmt.Println("tail",tail)
+	var l1Len, LastSum, l2Len, MaxLength, compareValue int
+	var initLinkNode *ListNode
+	//EndL2 = l2
+	//EndL1 = l1
 	//	制造2个数组，追加，and 获取两个链表的长度
 	var l1Arr, l2Arr []int
 	for l1 != nil {
@@ -64,48 +72,44 @@ func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 		l2 = l2.Next //移动指针到下一个链表
 		l2Len++
 	}
-	//Show(L1)
-	//Show(L2)
 	// 获取链表长度差值，数组末尾补 zero value
 	compareValue = int(math.Abs(float64(l2Len - l1Len)))
-	//fmt.Println("差值为",compareValue)
-	if len(l1Arr) == len(l2Arr) {
+	fmt.Println("差值为", compareValue)
+	if len(l1Arr) >= len(l2Arr) {
 		for i := 0; i < compareValue; i++ {
 			l2Arr = append(l2Arr, 0)
 		}
-		// 倒序
-		for i, j := 0, len(l2Arr)-1; i < j; i, j = i+1, j-1 {
-			l2Arr[i], l2Arr[j] = l2Arr[j], l2Arr[i]
-		}
-		// 从数组中抽出元素，追加到新的链表中
-		for _, i := range l2Arr {
-			var node = ListNode{Val: i} // 赋值给 tail 链表
-			node.Next = tail
-			tail = &node
-		}
-		EndL2 = tail
+		//// 倒序
+		//for i, j := 0, len(l2Arr)-1; i < j; i, j = i+1, j-1 {
+		//	l2Arr[i], l2Arr[j] = l2Arr[j], l2Arr[i]
+		//}
 	} else {
 		for i := 0; i < compareValue; i++ {
 			l1Arr = append(l1Arr, 0)
 		}
-		// 倒序
-		for i, j := 0, len(l1Arr)-1; i < j; i, j = i+1, j-1 {
-			l1Arr[i], l1Arr[j] = l1Arr[j], l1Arr[i]
-		}
-		// 从数组中抽出元素，追加到新的链表中
-		for _, i := range l1Arr {
-			var node = ListNode{Val: i} // 赋值给 tail 链表
-			node.Next = tail
-			tail = &node
-		}
-		EndL1 = tail
+		//// 倒序
+		//for i, j := 0, len(l1Arr)-1; i < j; i, j = i+1, j-1 {
+		//	l1Arr[i], l1Arr[j] = l1Arr[j], l1Arr[i]
+		//}
 	}
 	var res []int
-	for i := 0; i < len(l1Arr); i++ {
+	if l2Len > l1Len {
+		MaxLength = l2Len
+	} else {
+		MaxLength = l1Len
+	}
+	logrus.Info(MaxLength)
+	logrus.Warnf("1: %v \n 1: %v", l1Arr, l2Arr)
+	for i := 0; i < MaxLength; i++ {
 		if LastSum >= 10 {
-			LastSum = EndL1.Val + EndL2.Val + 1
+			if len(l1Arr) != 0 && len(l2Arr) != 0 {
+				LastSum = l1Arr[i] + l2Arr[i] + 1
+			}
+
 		} else {
-			LastSum = EndL1.Val + EndL2.Val
+			if len(l1Arr) != 0 && len(l2Arr) != 0 {
+				LastSum = l1Arr[i] + l2Arr[i]
+			}
 		}
 		if LastSum >= 10 {
 			//fmt.Println("当前sum为", LastSum)
@@ -114,9 +118,14 @@ func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 			//fmt.Println("当前sum为", LastSum)
 			res = append(res, LastSum)
 		}
-		//fmt.Printf(" 第%d轮value | SumInit ：%d |(%d + %d)/10 = %v \n", i, LastSum, EndL1.Val, EndL2.Val, res)
-		EndL1 = EndL1.Next
-		EndL2 = EndL2.Next
+		if len(l1Arr) != 0 && len(l2Arr) != 0 {
+			fmt.Printf(" 第%d轮value | SumInit ：%d |(%d + %d)/10 = %v \n", i, LastSum, l1Arr[i], l2Arr[i], res)
+		}
+		//if  EndL1 != nil || EndL2 != nil{
+		//	EndL1 = EndL1.Next
+		//	EndL2 = EndL2.Next
+		//}
+
 		// 判断最后一位，>= 10 ; 补 int 1
 		if i+1 == len(l1Arr) {
 			if LastSum >= 10 {
@@ -124,19 +133,7 @@ func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 			}
 		}
 	}
-	//logrus.Info(res)
-	// 将sum拆开塞入到一个新的链表中
-
-	////tail = l1
-	//var slice []int
-	//for _, i := range fmt.Sprintf("%d", iNum/10) {
-	//	//fmt.Println(i)
-	//	var a int
-	//	a, _ = strconv.Atoi(string(i))
-	//	//fmt.Println(a)
-	//	slice = append(slice, a)
-	//}
-	//fmt.Println(slice)
+	fmt.Println("end -------> ", res)
 	// 倒序
 	for i, j := 0, len(res)-1; i < j; i, j = i+1, j-1 {
 		res[i], res[j] = res[j], res[i]
