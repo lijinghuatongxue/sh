431b88a6ae0e2e4f0bc7dc254eb050a78f4e88d6
lijinghuatongxue <woshilijinghua@gmail.com>
2022-01-06 20:22:41 +0800

    leetcode: 整数数组判断是否存在重复元素，hash table 解法

--- leetcode/contains-duplicate_leetcode.go
+++ leetcode/contains-duplicate_leetcode.go
@@ -3,7 +3,7 @@ package main
 import "log"
 
 // https://leetcode-cn.com/problems/contains-duplicate/solution/cun-zai-zhong-fu-yuan-su-by-leetcode-sol-iedd/
-// hash table  解法
+// hash  table  解法
 func containsDuplicateLeetcode(nums []int) bool {
 	set := map[int]struct{}{}
 	for _, i := range nums {
