9d9a4d8f8c8c44477b9c469ac310b51c59a298f2
lijinghuatongxue <woshilijinghua@gmail.com>
2022-03-01 22:42:05 +0800

    leetbook: remove array Duplicates

--- leetcode/array/removeDuplicates.go
+++ leetcode/array/removeDuplicates.go
@@ -5,10 +5,25 @@ import (
 )
 
 func main() {
-	nums := []int{1, 1}
+	nums := []int{1, 1, 3, 4, 5, 5}
 	fmt.Println(RemoveDuplicates(nums))
+	fmt.Println(RemoveDuplicatesTwo(nums))
 }
 func RemoveDuplicates(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	slow := 1
+	for fast := 1; fast < len(nums); fast++ {
+		if nums[fast] != nums[fast-1] {
+			nums[slow] = nums[fast]
+			slow++
+		}
+	}
+	return slow
+}
+
+func RemoveDuplicatesTwo(nums []int) int {
 	if len(nums) == 0 {
 		return 0
 	}
