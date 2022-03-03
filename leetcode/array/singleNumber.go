f94060b8288815c73785057a058bcca0f352c2d5
lijinghuatongxue <woshilijinghua@gmail.com>
2022-03-03 23:28:26 +0800

    leetbook: bit Operation

--- leetcode/array/singleNumber.go
+++ leetcode/array/singleNumber.go
@@ -5,10 +5,13 @@ import (
 )
 
 func main() {
-	s := fmt.Sprintf("%d", singleNumber([]int{1, 2, 6, 6, 1, 7, 7}))
+	s := fmt.Sprintf("%d", singleNumberBitOperation([]int{1, 2, 6, 6, 1, 7, 7}))
 	fmt.Println(s)
 }
-func singleNumber(nums []int) int {
+func singleNumberBitOperation(nums []int) int {
+	//任何数和自己做异或运算，结果为 0; a^a=0
+	//任何数和 0 做异或运算，结果还是自己; a^0 = a
+	//异或运算中，满足交换律和结合律，也就是a^b^a = b^a^a = b^(a^a) = b^0 = b
 	var res int
 	for _, v := range nums {
 		res = res ^ v
