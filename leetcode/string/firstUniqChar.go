3a82eab3e6b24b9488810cf981976e3c7a3a5b75
lijinghuatongxue <598820383@qq.com>
2022-03-14 23:28:01 +0800

    leetbook:  字符串中的第一个唯一字符 https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/xn5z8r/

--- leetcode/string/firstUniqChar.go
+++ leetcode/string/firstUniqChar.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-// 字符串中的第一个唯一字符
+// 给定一个字符串 s ，找到 它的第一个不重复的字符，并返回它的索引 。如果不存在，则返回 -1 。
 // https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/xn5z8r/
 func main() {
 	fmt.Println(firstUniqChar("liuqi"))
