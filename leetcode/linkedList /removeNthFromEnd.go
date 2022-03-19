9a9ec35590387a6bf21b536d1192d9d249aa1f0d
li.jinghua <li.jinghua@immomo.com>
2022-03-19 23:35:53 +0800

    leetbook : 删除链表的倒数第N个节点 https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/xn2925/

---
+++ leetcode/linkedList /removeNthFromEnd.go
@@ -0,0 +1,24 @@
+package main
+
+func main() {
+	var head = new(ListNode)
+	removeNthFromEnd(head, 1)
+}
+func removeNthFromEnd(head *ListNode, n int) *ListNode {
+	length := getLength(head)
+	dummy := &ListNode{0, head}
+	cur := dummy
+	for i := 0; i < length-n; i++ {
+		cur = cur.Next
+	}
+	cur.Next = cur.Next.Next
+	return dummy.Next
+}
+
+// 计算链表长度
+func getLength(head *ListNode) (length int) {
+	for ; head != nil; head = head.Next {
+		length++
+	}
+	return
+}
