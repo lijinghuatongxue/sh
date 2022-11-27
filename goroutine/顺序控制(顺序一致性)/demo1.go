15425f4bd9d2ff6a908c2acd8602a8ffea5b07cc
woshilijinghua <woshilijinghua@gmail.com>
2022-11-27 23:13:23 +0800

    顺序一致性：顺序控制

--- goroutine/顺序控制(顺序一致性)/demo1.go
+++ goroutine/顺序控制(顺序一致性)/demo1.go
@@ -11,5 +11,5 @@ func main() {
 		fmt.Println("hello world")
 		mu.Unlock()
 	}()
-	mu.Lock() // 执行到此行时，mu.Unlock() 已经执行，因为同一个线程的顺序可以保证，所有此时打印事件已经发生
+	mu.Lock() // 执行到此行时，mu.Unlock() 已经执行，因为同一个线程的顺序可以保证，所以此时打印事件已经发生
 }
