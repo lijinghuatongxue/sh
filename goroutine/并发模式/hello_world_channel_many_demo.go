16d40477ebc7c2d1024cd965226cec280cd381de
woshilijinghua <woshilijinghua@gmail.com>
2022-11-27 23:44:02 +0800

    并发模式：chan

--- goroutine/并发模式/hello_world_channel_many_demo.go
+++ goroutine/并发模式/hello_world_channel_many_demo.go
@@ -1,9 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/sirupsen/logrus"
+	"time"
+)
 
 func main() {
 	var done = make(chan int, 10)
+	logrus.Infof("chal.cap.is:%v", cap(done))
 	for i := 0; i < cap(done); i++ {
 		go func() {
 			// fmt
@@ -12,6 +17,7 @@ func main() {
 		done <- 1
 	}
 	for i := 0; i < cap(done); i++ {
+		time.Sleep(1 * time.Second) // 这里出现等待，两个for同时运行，没有将通道消费完，程序不停止
 		<-done
 	}
 }
